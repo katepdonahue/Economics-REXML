2922b4de84fa49316a0227d70eb9607de92d2db3
Kate <katepdonahue@gmail.com>
2013-12-06 12:11:01 -0500

    Initial commit

---
+++ economics.rb
@@ -0,0 +1,15 @@
+require 'rexml/document'
+file = File.new( "cia-1996.xml" ) #is this repetitive if I already have a file?
+doc = REXML::Document.new file
+
+def population
+
+end
+
+def inflation_rates
+
+end
+
+def continent_hash
+
+end
---
+++ spec.rb
@@ -0,0 +1,20 @@
+require './economics'
+
+describe "#population" do
+  it "should return a sorted hash with country keys and population values" do
+
+  end
+end
+
+describe "#inflation_rates" do
+  it "should return a sorted hash with country keys and inflation rates values" do
+
+  end
+end
+
+describe "#continent_hash" do
+  it "should return a sorted hash with continent keys and country values" do
+
+  end
+end
+
